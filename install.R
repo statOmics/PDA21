89db6d052ec2682208d8b9b73d44e6622febb8b2
lieven clement <lclement@lievens-MacBook-Pro.local>
2021-06-24 13:42:35 +0200

    new libs to render to pdf

--- install.R
+++ install.R
@@ -8,6 +8,10 @@ if (!require("BiocManager"))
 
 ## ----pkgs----------------------------------------------------------------
 pkgs <- c("tidyverse",
+          "devtools", 
+          "rmarkdown", 
+          "knitr",
+          "latex2exp",
           "limma",
           "plotly",
           "QFeatures",
